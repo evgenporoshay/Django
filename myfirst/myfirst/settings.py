f7b9d0cadb12b868e802b2b68717ed39e6115a4b
poroshchayea <bijyji.424>
2022-06-22 19:54:11 +0300

    change imports...

--- myfirst/myfirst/settings.py
+++ myfirst/myfirst/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os.path
-from pathlib import Path
+import pathlib
 import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
